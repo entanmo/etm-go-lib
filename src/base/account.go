421fb2ce1ae760412d9ce6a4b7ece3a8725ed49f
SyAtlantis <1102960696@qq.com>
2019-05-21 11:24:53 +0800

    添加account和block模块，未完成

--- src/base/account.go
+++ src/base/account.go
@@ -1,7 +1,10 @@
 package base
 
 import (
+	"fmt"
 	"reflect"
+	"crypto/sha256"
+	"workspace/etm-go-lib/src/utils"
 )
 
 type Account struct {
@@ -10,4 +13,20 @@ type Account struct {
 
 func (a Account) IsEmpty() bool {
 	return reflect.DeepEqual(a, Account{})
+}
+
+func (a Account) GetKeypairBySecret(secret string) (string, string) {
+	hash := sha256.Sum256([]byte(secret))
+	ed := utils.Ed{}
+	keypair := ed.MakeKeypair(hash[:])
+	
+	publicKey := fmt.Sprintf("%x", keypair.PublicKey)
+	privateKey := fmt.Sprintf("%x", keypair.PrivateKey)
+	
+	return publicKey, privateKey
+}
+
+func (a Account) GetAddressByPublicKey(publicKey []byte) string {
+	address := utils.Address{}
+	return address.GenerateAddresss(publicKey)
 }
--- src/base/block.go
+++ src/base/block.go
@@ -1,1 +1,50 @@
 package base
+
+import (
+	"fmt"
+	"bytes"
+	"crypto/sha256"
+	"workspace/etm-go-lib/src/utils"
+)
+
+type Block struct {
+	Id                   string        `json:"id"`
+	Version              string        `json:"version"`
+	TotalAmount          int64         `json:"totalAmount"`
+	TotalFee             int64         `json:"totalFee"`
+	Reward               int64         `json:"reward"`
+	PayloadHash          string        `json:"payloadHash"`
+	Timestamp            int64         `json:"timestamp"`
+	NumberOfTransactions int           `json:"NnumberOfTransactions"`
+	PayloadLength        int64         `json:"payloadLength"`
+	PreviousBlock        string        `json:"previousBlock"`
+	GeneratorPublicKey   string        `json:"generatorPublicKey"`
+	Transactions         []Transaction `json:"transactions"`
+}
+
+func (block *Block) Create(data UserData) {
+
+}
+
+func (block *Block) GetBytes() []byte {
+	bb := bytes.NewBuffer([]byte{})
+	
+	return bb.Bytes()
+}
+
+func (block *Block) GetHash() [32]byte {
+	_bytes := block.GetBytes()
+	_hash := sha256.Sum256(_bytes)
+	return _hash
+}
+
+func (block *Block) GetId() string {
+	return fmt.Sprintf("%x", block.GetHash())
+}
+
+func (block *Block) GetSignature(keypair utils.Keypair) string {
+	_hash := block.GetHash()
+	ed := utils.Ed{}
+	_sign := ed.Sign(_hash[:], keypair)
+	return fmt.Sprintf("%x", _sign)
+}
